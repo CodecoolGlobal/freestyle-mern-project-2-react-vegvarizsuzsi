43296032bd99f9ba7de770baece21355ec0f5e59
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2023-11-03 22:23:04 +0100

    delete empty spaces, add birth year usestate, add it into the return statement

--- frontend/src/components/CharacterItem.jsx
+++ frontend/src/components/CharacterItem.jsx
@@ -2,20 +2,17 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import './CharacterCards.css';
 
-
-
 const deleteCharacter = (id) => {
   return fetch(`/api/characters/${id}`, { method: "DELETE" }).then((res) => res.json())
-
 };
 
 function CharacterItem({ character }) {  //characters --> nem kellenek a setterek
   const [name,] = useState(character.name);
-  
+  const [birthYear,] = useState(character.birthYear);
   const [height,] = useState(character.height)
   const [mass,] = useState(character.mass);
   const [gender,] = useState(character.gender);
-  const [img, setImg] = useState(character.img);
+  const [img,] = useState(character.img);
   const [, setCharacters] = useState([])
 
 
@@ -23,7 +20,6 @@ function CharacterItem({ character }) {  //characters --> nem kellenek a settere
     const confirmed = window.confirm('Are you sure you want to delete this character?')
 
     if (confirmed) {
-
       try {
         await deleteCharacter(id);
         setCharacters((characters) => {
@@ -40,8 +36,6 @@ function CharacterItem({ character }) {  //characters --> nem kellenek a settere
   }
 
   return (
-   
-
       <div className="card">
         <img src={img} alt='' className="img"
           style={{
@@ -49,7 +43,8 @@ function CharacterItem({ character }) {  //characters --> nem kellenek a settere
             height: 200,
             width: 170
           }} />
-        <h2>{name}</h2>     {/*  map-elés utána csak ch.name ...  */}      
+        <h2>{name}</h2>
+        <p>Birth year: {birthYear}</p>
         <p>Height: {height} cm</p>
         <p>Mass: {mass} kg</p>
         <p>Gender: {gender}</p>
@@ -58,11 +53,6 @@ function CharacterItem({ character }) {  //characters --> nem kellenek a settere
         </Link>
         <button type="button" onClick={() => handleDelete(character._id)}>Delete</button>
       </div>
-
-    
-
-
-
   )
 }
 
