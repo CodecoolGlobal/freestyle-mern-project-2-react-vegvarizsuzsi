1764e9bda8735171fe3fe333053c6f190061c5b4
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2023-11-03 22:03:45 +0100

    refactor code, add try catch block, create fetchData function to fetch nicer

--- backend/fetch_swapi/characters.js
+++ backend/fetch_swapi/characters.js
@@ -1,38 +1,45 @@
 require('dotenv').config();
 
+const { exit } = require('node:process');
 const fetch = require('node-fetch');
 const mongoose = require('mongoose');
 const People = require('../models/characterModel');
 
-mongoose.connect(
-    process.env.MONGO_URL
-)
+async function fetchData() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
 
-    .then(() => console.log('connected'))
-    .then(() => getPeople(1))
-    .then(() => getPeople(2))
-    .then(() => getPeople(3))
-    .then(() => getPeople(4))
-    .then(() => getPeople(5))
-    .then(() => getPeople(6))
-    .then(() => getPeople(7))
-    .then(() => getPeople(8))
-    .then(() => getPeople(9))
-    .catch(e => console.log(e));
+    for (let i = 1; i <= 9; i++) {
+      await getPeople(i);
+    }
+    console.log('All data saved successfully');
+    exit(0);
+  } catch (error) {
+    console.error('Error:', error);
+    exit(1);
+  }
+}
 
 async function getPeople(id) {
+  try {
     const getCharacter = await fetch(`https://swapi.dev/api/people/?page=${id}`);
     const response = await getCharacter.json();
 
     for (let i = 0; i < response.results.length; i++) {
-        const people = new People({
-            name: response.results[i].name,
-            birthYear: response.results[i].birth_year,
-            height: response.results[i].height,
-            mass: response.results[i].mass,
-            gender: response.results[i].gender
-        })
+      const people = new People({
+        name: response.results[i].name,
+        birthYear: response.results[i].birth_year,
+        height: response.results[i].height,
+        mass: response.results[i].mass,
+        gender: response.results[i].gender,
+      });
 
-        await people.save()
+      await people.save();
     }
+    console.log(`Saved people for id ${id}`);
+  } catch (error) {
+    console.error(`Error occurred while fetching ${id}: `, error);
+  }
 }
+
+fetchData();
